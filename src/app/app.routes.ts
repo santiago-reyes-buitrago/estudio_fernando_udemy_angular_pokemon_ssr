67799d96418da4fb376a56831adc4306eca4b30d
Santiago Reyes <santiago.reyes@platinum.net.co>
2025-10-29 09:17:56 -0500

    inicio seccion 5 - se salta seccion de testing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about-page/about-page')
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./pages/contact-page/contact-page')
+  },
+  {
+    path: 'princing',
+    loadComponent: () => import('./pages/pricing-page/pricing-page')
+  },
+  {
+    path: '**',
+    redirectTo: () => 'about'
+  }
+];
---
+++ src/app/pages/contact-page/contact-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'contact-page',
+  imports: [],
+  templateUrl: './contact-page.html',
+  styleUrl: './contact-page.css',
+})
+export default class ContactPage {
+
+}
