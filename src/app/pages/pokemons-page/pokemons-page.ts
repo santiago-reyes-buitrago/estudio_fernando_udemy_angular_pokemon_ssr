f9760f2f342dc5f20e085933213c203547b80d55
Santiago Reyes <santiago.reyes@platinum.net.co>
2025-11-24 10:22:29 -0500

    Iniciando seccion 6 - Angular pro

---
+++ src/app/pages/pokemons-page/pokemons-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pokemons-page',
+  imports: [],
+  templateUrl: './pokemons-page.html',
+  styleUrl: './pokemons-page.css',
+})
+export default class PokemonsPage {
+
+}
