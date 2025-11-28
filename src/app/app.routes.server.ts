57de1d3fb79805b4be167002ad3a7d3e440efc17
Santiago Reyes <santiago.reyes@platinum.net.co>
2025-11-28 11:54:41 -0500

    Arreglos en ssr por incompatibilidad angular V20

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,4 +1,8 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import {Result} from './pokemons/interfaces';
+
+const TOTAL_POKEMONS = 50;
+const TOTAL_PAGES = 30;
 
 const {Server,Prerender,Client} = RenderMode
 
@@ -14,6 +18,28 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: 'princing',
     renderMode: Prerender
+  },
+  {
+    path: 'pokemons/page/:page',
+    renderMode: Prerender,
+    async getPrerenderParams() {
+      const pokemonsPages = Array.from({length: TOTAL_PAGES}, (_, i) => i + 1)
+        // .map((id) => `/pokemons/page/${id}`)
+      return pokemonsPages.map(item => ({page: `${item}`}))
+    }
+  },
+  {
+    path: 'pokemon/:id',
+    renderMode: Prerender,
+    async getPrerenderParams() {
+      const pokemonsIds = Array.from({length: TOTAL_POKEMONS}, (_, i) => i + 1)
+        // .map((id) => `/pokemon/${id}`)
+
+      const {results: pokemonsNameList} = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${TOTAL_POKEMONS}`).then(res => res.json())
+      return [...pokemonsIds,...pokemonsNameList.map((pokemon:Result) => pokemon.name)].map(id => ({
+        id: `${id}`
+      }))
+    }
   },
   {
     path: '**',
